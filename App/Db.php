286dbd3091bd7c7b03c088ad471941030e237bb8
Nikoloz <test@mail.ru>
2016-04-23 20:50:14 +0300

    + Db.php

---
+++ App/Db.php
@@ -0,0 +1,25 @@
+<?php
+namespace App;
+class Db
+{
+    protected $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=frame;charset=utf8','root','');
+    }
+
+    public function execute($sql)
+    {
+        return $this->dbh->prepare($sql)->execute();
+    }
+
+    public function query($sql, $class)
+    {
+        $sth = $this->dbh->prepare($sql);
+        if(false !== $sth->execute()) {
+            return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
+        }
+        return [];
+    }
+}
